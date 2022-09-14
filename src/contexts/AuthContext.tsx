7c3d74ccb8169eaa65c9a1f54f4f37f52c266b0c
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-09-14 08:31:19 +0700

    clear

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,87 +1,37 @@
-import { createContext, ReactNode, useReducer } from "react";
-import { authReducer, AuthState } from "../reducers/AuthReducer";
-import { AuthActionTpye } from "../reducers/type";
-
-const { TOGGLE_AUTH } = AuthActionTpye;
-
-interface AuthContextProps {
-  children: ReactNode;
-}
-
-interface AuthContextDefault {
-  authInfo: AuthState;
-  toggleAuth: (useName: string) => void;
-}
-const authDefault = {
-  isAuthenticated: false,
-  useName: '',
-}
-
-export const AuthContext = createContext<AuthContextDefault>({
-  authInfo: authDefault,
-  toggleAuth: () => {},
-});
-
-const AuthContextProvider = ({ children }: AuthContextProps) => {
-  const [authInfo, dispatch] = useReducer(authReducer, authDefault);
-
-  const toggleAuth = (username: string) =>
-    dispatch({ type: TOGGLE_AUTH, payload: username });
-
-  const authContextData = {
-    authInfo,
-    toggleAuth,
-  }
-
-  return (
-    <AuthContext.Provider value={authContextData}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export default AuthContextProvider;
-
-
-
-
 // import { createContext, ReactNode, useReducer } from "react";
 // import { authReducer, AuthState } from "../reducers/AuthReducer";
 // import { AuthActionTpye } from "../reducers/type";
+
+// const { TOGGLE_AUTH } = AuthActionTpye;
 
 // interface AuthContextProps {
 //   children: ReactNode;
 // }
 
-// const { TOGGLE_AUTH } = AuthActionTpye;
-
 // interface AuthContextDefault {
 //   authInfo: AuthState;
-//   toggleAuth: (username: string) => void;
+//   toggleAuth: (useName: string) => void;
 // }
-
-// const authDefault: AuthState = {
+// const authDefault = {
 //   isAuthenticated: false,
-//   userName: "",
-// };
+//   useName: '',
+// }
 
 // export const AuthContext = createContext<AuthContextDefault>({
 //   authInfo: authDefault,
-//   toggleAuth: () => null,
+//   toggleAuth: () => {},
 // });
 
 // const AuthContextProvider = ({ children }: AuthContextProps) => {
 //   const [authInfo, dispatch] = useReducer(authReducer, authDefault);
 
-//   const toggleAuth = (username: string) => {
-//     console.log(username);
+//   const toggleAuth = (username: string) =>
 //     dispatch({ type: TOGGLE_AUTH, payload: username });
-//   };
 
 //   const authContextData = {
 //     authInfo,
 //     toggleAuth,
-//   };
+//   }
 
 //   return (
 //     <AuthContext.Provider value={authContextData}>
@@ -91,3 +41,53 @@ export default AuthContextProvider;
 // };
 
 // export default AuthContextProvider;
+
+
+
+
+import { createContext, ReactNode, useReducer } from "react";
+import { authReducer, AuthState } from "../reducers/AuthReducer";
+import { AuthActionTpye } from "../reducers/type";
+
+interface AuthContextProps {
+  children: ReactNode;
+}
+
+const { TOGGLE_AUTH } = AuthActionTpye;
+
+interface AuthContextDefault {
+  authInfo: AuthState;
+  toggleAuth: (username: string) => void;
+}
+
+const authDefault: AuthState = {
+  isAuthenticated: false,
+  userName: "",
+};
+
+export const AuthContext = createContext<AuthContextDefault>({
+  authInfo: authDefault,
+  toggleAuth: () => null,
+});
+
+const AuthContextProvider = ({ children }: AuthContextProps) => {
+  const [authInfo, dispatch] = useReducer(authReducer, authDefault);
+
+  const toggleAuth = (username: string) => {
+    console.log(username);
+    dispatch({ type: TOGGLE_AUTH, payload: username });
+  };
+
+  const authContextData = {
+    authInfo,
+    toggleAuth,
+  };
+
+  return (
+    <AuthContext.Provider value={authContextData}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
